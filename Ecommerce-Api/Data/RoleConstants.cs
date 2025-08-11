52180015494cd34f9c7c35a890353f95c839a184
minahilismail <minahilismail44@gmail.com>
2025-08-11 17:19:12 +0500

    refactor:remove unused class

--- Ecommerce-Api/Data/RoleConstants.cs
+++
@@ -1,9 +0,0 @@
-namespace Ecommerce_Api.Models
-{
-    public static class RoleConstants
-    {
-        public const string User = "User";
-        public const string Seller = "Seller";
-        public const string Admin = "Administrator";
-    }
-}
